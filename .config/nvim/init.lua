ab56435c555903023d7571f00f446e2f0cdf3b2e
veplix <127900312+veplix@users.noreply.github.com>
2025-10-14 21:12:51 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,8 +2,4 @@ require("config.lazy")
 vim.opt.termguicolors = false
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.keymap.set({ 'n', 'v' }, 'y', '"+y', { noremap = true })
-vim.keymap.set({ 'n', 'v' }, 'y', '"+Y', { noremap = true })
-vim.keymap.set({ 'n', 'v' }, 'p', '"+p', { noremap = true })
-vim.keymap.set({ 'n', 'v' }, 'P', '"+P', { noremap = true })
-vim.opt.clipboard = ""
+vim.opt.clipboard = "unnamedplus"
