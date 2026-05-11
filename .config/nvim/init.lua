fe3718ea633c60267c710f52964300fe5403ddb6
veplix <127900312+veplix@users.noreply.github.com>
2026-05-11 12:08:24 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,3 +4,6 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.showmode = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
